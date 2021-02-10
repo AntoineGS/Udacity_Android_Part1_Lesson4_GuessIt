9f20cc1e15256ff6bb3570901a627ff340432a19
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-02-09 20:18:15 -0500

    use mutableLiveData as an event to complete the game

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,11 @@ class GameViewModel : ViewModel() {
     val score : LiveData<Int>
         get() = _score
 
+    // Event for game end
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish : LiveData<Boolean>
+        get() = _eventGameFinish
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -25,6 +30,7 @@ class GameViewModel : ViewModel() {
         nextWord()
         _score.value = 0
         _word.value = ""
+        _eventGameFinish.value = false
         Timber.i("GameViewModel created!")
     }
 
@@ -70,7 +76,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-//            gameFinished()
+            _eventGameFinish.value = true
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -84,5 +90,9 @@ class GameViewModel : ViewModel() {
     fun onCorrect() {
         _score.value = _score.value?.plus(1)
         nextWord()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
     }
 }
